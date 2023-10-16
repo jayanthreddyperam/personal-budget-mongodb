30343004c058131de176eea635cf8c79d2d60633
jayanthreddyperam <jayanthperam444@gmail.com>
2023-10-15 23:20:36 -0400

    Added Fetching data&End Points

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const Budget = require('./budget'); // Import the Mongoose model
+
+app.use(bodyParser.json());
+
+// Endpoint to get budget data from the database
+app.get('/budget', (req, res) => {
+  Budget.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+// Endpoint to add new budget data to the database
+app.post('/budget', (req, res) => {
+    const { title, value, color } = req.body;
+    const budgetEntry = new Budget({ title, value, color });
+  
+    budgetEntry.save((err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(201).json(data);
+      }
+    });
+});
 
 app.use('/', express.static('public'));
 
-const budget = require('./budget.json');
-app.get('/budget', (req, res) => {
-    res.json(budget);
-});
+// const budget = require('./budget.json');
+// app.get('/budget', (req, res) => {
+//     res.json(budget);
+// });
 
-app.get('/hello', (req,res) => {
-    res.send('Hello World!');
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
